7990d9d3291ed2035d39f2799f644d7c1eb0019f
Burak <6028083@mborijnland.nl>
2024-10-24 12:01:17 +0200

    post_2

--- classes/Post.php
+++ classes/Post.php
@@ -7,4 +7,14 @@ class Post
     {
         $this->db = $db;
     }
+
+    public function getAllPosts($page, $postsPerPage)
+    {
+        $offset = ($page - 1) * $postsPerPage;
+        $stmt = $this->db->getPdo()->prepare("SELECT * FROM posts ORDER BY created_at DESC LIMIT :limit OFFSET :offset");
+        $stmt->bindValue(':limit', $postsPerPage, PDO::PARAM_INT);
+        $stmt->bindValue(':offset', $offset, PDO::PARAM_INT);
+        $stmt->execute();
+        return $stmt->fetchAll();
+    }
 }
