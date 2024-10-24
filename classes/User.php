9873e444f3f091a5a8c0c01f6faa9cb02d9c4634
Burak <6028083@mborijnland.nl>
2024-10-24 12:08:47 +0200

    user_2

--- classes/User.php
+++ classes/User.php
@@ -7,4 +7,19 @@ class User
     {
         $this->db = $db;
     }
+
+    public function login($username, $password)
+    {
+        $stmt = $this->db->getPdo()->prepare("SELECT users.*, roles.name as role_name FROM users JOIN roles ON users.role_id = roles.id WHERE username = ?");
+        $stmt->execute([$username]);
+        $user = $stmt->fetch();
+
+        if ($user && password_verify($password, $user['password'])) {
+            $_SESSION['user_id'] = $user['id'];
+            $_SESSION['username'] = $user['username'];
+            $_SESSION['role'] = $user['role_name'];
+            return true;
+        }
+        return false;
+    }
 }
