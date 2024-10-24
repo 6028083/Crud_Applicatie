5cd54d470469bd6ebfde857491f6f20f010d982d
Burak <6028083@mborijnland.nl>
2024-10-24 12:11:38 +0200

    user_5

--- classes/User.php
+++ classes/User.php
@@ -33,4 +33,10 @@ class User
         $stmt = $this->db->getPdo()->query("SELECT users.*, roles.name as role_name FROM users JOIN roles ON users.role_id = roles.id");
         return $stmt->fetchAll();
     }
+
+    public function changeUserRole($userId, $newRoleId)
+    {
+        $stmt = $this->db->getPdo()->prepare("UPDATE users SET role_id = ? WHERE id = ?");
+        return $stmt->execute([$newRoleId, $userId]);
+    }
 }
