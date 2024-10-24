4740785e0e61fd9d272a14f021d06a2f708c54ee
Burak <6028083@mborijnland.nl>
2024-10-24 11:54:16 +0200

    config push

--- config.php
+++ config.php
@@ -3,3 +3,10 @@ $host = 'localhost';
 $dbname = 'blog_db';
 $username = 'root';
 $password = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Databaseverbinding mislukt: " . $e->getMessage());
+}
